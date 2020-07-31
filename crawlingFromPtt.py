ec0afec7b337fc08f94c7ee1f22537229528ee95
poting <chou830702@gmail.com>
2020-07-30 22:08:05 -0700

    Crawl all posts relate to airpods transactions from PTT Headphone community and store in local txt file

--- crawlingFromPtt.py
+++ crawlingFromPtt.py
@@ -6,28 +6,29 @@ import time
 def sleepTime(hour, min, sec):
     return hour*3600 + min*60 + sec
 
-
-startTime = time.time()
-file = io.open("Biding Price.txt", "ab+")
-i = 1
-link = "https://www.ptt.cc/bbs/Headphone/search?page=1&q=airpods"
-req = rq.get(link)
-soup = BeautifulSoup(req.text, "lxml")
-print (soup.find_all('a')[10].get_text())
-postTitle = soup.find_all('a')[10].get_text()
-if "問題" in postTitle:
-    print("exist")
-
-"""
-while (i <= 10):
-    link = "https://www.ptt.cc/bbs/Headphone/search?page=" + str(i) + "&q=airpods"
-    req = rq.get(link)
-    soup = BeautifulSoup(req.text, "lxml")
-    for url in soup.findAll('a', {'class': 'brand_name'})
-        response = rq.get(url.get('href'))
-        htmlDoc = response.text
-        soup = BeautifulSoup(response.text. 'lxml')
-        if soup.select('a') ! = []:
-            seller = 
-"""
+doc = io.open("Biding Price.txt", "ab+") # create file to document the data, 
+#ab+ : if file exist, point to the end. If not create a new one
+i = 1 # start from the first page
+while (i < 5): # max 4 pages
+    link = "https://www.ptt.cc/bbs/Headphone/search?page=" + str(i) + "&q=airpods" # target link
+    req = rq.get(link) # Use GET method
+    if req.status_code == 200: # if request success
+        soup = BeautifulSoup(req.text, "lxml") #change to lxml
+        for post in soup.find_all('a'): # find all <a> tag 
+            postTitle = post.get_text() # get the title of the post
+            if "交易" in postTitle: # If the post is categorized as 'transaction'
+                postLink = "https://www.ptt.cc" + post.get('href') # link of the current post
+                time.sleep(3) # wait time, prevent IP blocking because of frequently request 
+                contReq = rq.get(postLink) # HTTP get request
+                soupCont = BeautifulSoup(contReq.text, "lxml")
+                content = soupCont.find(id="main-content").get_text() # get the text of content by ID of HTML tag
+                if "價格" in content: # if the "價錢" character includes in the text, write the content to a local txt file
+                    doc.write(content.encode('utf-8') + '==============================='.encode('utf-8'))  # separate posts
+                    doc.write('\n'.encode('utf-8')) # change line
+                    time.sleep(sleeptime(0,0,3)) # sleepTime. Prevent IP blocking
+    else :
+        print("No More Pages") # status code != 200. Pages not exist
+    i = i + 1
+doc.close() # io connection close
+print("ALL DONE")    
 
